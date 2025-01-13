cee30d3087db6fd590f2f8ddc84d55e32f05a642
Nikita <6stringsohei@gmail.com>
2025-01-13 22:43:27 +0300

    local auth naive implementation

--- app/landing/header/header.tsx
+++ app/landing/header/header.tsx
@@ -35,7 +35,7 @@ export function Header() {
 
 
                     <a
-                        href={"#Login"}
+                        href={"http://localhost:3030/auth/login"}
                         className="border border-green-500 text-green-500 px-4 py-2 rounded hover:bg-green-500 hover:text-black transition-all duration-300">
                         Log in
                     </a>
