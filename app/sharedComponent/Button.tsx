b514967d99742b5d53b492cea0e10bc326acd04c
Nikita <6stringsohei@gmail.com>
2025-02-02 20:36:27 +0300

    Button component with customizable classname

--- app/sharedComponent/Button.tsx
+++ app/sharedComponent/Button.tsx
@@ -3,15 +3,19 @@ import React from "react";
 interface ButtonProps {
     onClick?: React.MouseEventHandler<HTMLAnchorElement>;
     label?: string;
+    className?: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({ onClick, label = "Log in" }) => {
+export const Button: React.FC<ButtonProps> = ({ onClick, label = "Log in", className = "" }) => {
     return (
         <a
             onClick={onClick}
-            className="border border-green-500 text-green-500 text-center px-6 py-3 rounded-lg
-            hover:bg-green-500 hover:text-black transition-all duration-300 cursor-pointer
-            flex justify-center items-center w-full max-w-[12rem]"
+            className={`
+                border border-green-500 text-green-500 text-center px-6 py-3 rounded-lg
+                hover:bg-green-500 hover:text-black transition-all duration-300 cursor-pointer
+                flex justify-center items-center w-full max-w-[12rem]
+                ${className}
+            `.trim()}
         >
             {label}
         </a>
