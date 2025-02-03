7623147b9f910ae64e9af496f2ce827437dc3989
Nikita <6stringsohei@gmail.com>
2025-02-03 17:36:27 +0300

    fixed button component

--- app/sharedComponent/Button.tsx
+++ app/sharedComponent/Button.tsx
@@ -1,25 +1,34 @@
 import React from "react";
 
 interface ButtonProps {
-    onClick?: React.MouseEventHandler<HTMLAnchorElement>;
+    as?: "button" | "a";
+    href?: string;
+    onClick?: React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>;
     label?: string;
     className?: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({onClick, label = "Select", className = ""}) => {
-    return (
-        <div className={"flex justify-center w-full"}>
-            <a
-                onClick={onClick}
-                className={`
-                border border-green-500 text-green-500 text-center px-6 py-3 rounded-lg
-                hover:bg-green-500 hover:text-black transition-all duration-300 cursor-pointer
-                flex justify-center items-center w-full max-w-[12rem]
-                ${className}
-            `.trim()}
-            >
-                {label}
-            </a>
-        </div>
+export const Button: React.FC<ButtonProps> = ({
+                                                  as = "button",
+                                                  href,
+                                                  onClick,
+                                                  label = "Select",
+                                                  className = "",
+                                              }) => {
+    const commonClasses = `
+        border border-green-500 text-green-500 text-center px-6 py-3 rounded-lg
+        hover:bg-green-500 hover:text-black transition-all duration-300 cursor-pointer
+        flex justify-center items-center
+        ${className}
+    `.trim();
+
+    return as === "a" && href ? (
+        <a href={href} className={commonClasses} onClick={onClick}>
+            {label}
+        </a>
+    ) : (
+        <button onClick={onClick} className={commonClasses}>
+            {label}
+        </button>
     );
 };
